6b24d54c13b4c7b48bf3b4f57a3f69c5b20c30e5
Młyński Stanisław <mlynski.stanislaw@gmail.com>
2020-01-16 16:36:54 +0100

    Dodaj pliki projektów.

---
+++ Classes/Class1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    class Class1
+    {
+        public static int Count = 0;
+        public string nazwa;
+        public double atak;
+
+        public Class1()
+        {
+            atak = 3454.345;
+            nazwa = "PrzykładowaNazwa";
+            Count++;
+            //Obrona = obrona;
+        }
+        public Class1(string nazwa_, double atak_)
+        {
+            nazwa = nazwa_;
+            atak = atak_;
+            Count++;
+        }
+        public void Wypisz()
+        {
+            Console.WriteLine(nazwa);
+            Console.WriteLine(atak);
+        }
+        //public double Atak { get; set; }
+        //public double Obrona { get; set; }
+        
+    }
+}
---
+++ Classes/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 przykladowabron = new Class1();
+            przykladowabron.Wypisz();
+            Console.WriteLine();
+
+            Class1 innabron = new Class1("podgatunek innej broni",34.43);
+            innabron.Wypisz();
+            Console.WriteLine();
+
+            Class1 innabron2 = new Class1("inny podgatunek", 45.54);
+            innabron.Wypisz();
+
+            Console.Write("Ilość rodzajów broni:" + Class1.Count);
+
+            Console.ReadKey();
+        }
+    }
+}
